2fefc05d9cd04caacb9c7abc23a3390230efc80b
basil-wecodx <basil@wecodx.com>
2023-05-10 14:52:36 +0200

    contact.vue modif du css pour le mettre sous format tw

--- electric-city/client/pages/contact.vue
+++ electric-city/client/pages/contact.vue
@@ -2,110 +2,110 @@
 	<section class="relative">
 		<img
 			src="StudioFond.jpg"
-			class="absolute -z-20 top-0 left-0 h-full w-full object-cover brightness-75"
+			class="tw-absolute -tw-z-20 tw-top-0 tw-left-0 tw-h-full tw-w-full tw-object-cover tw-brightness-75"
 		/>
 		<div
-			class="absolute -z-10 h-full w-full bg-gradient-to-b from-transparent via-transparent to-black"
+			class="tw-absolute -tw-z-10 tw-h-full tw-w-full tw-bg-gradient-to-b tw-from-transparent tw-via-transparent tw-to-black"
 		></div>
-		<div class="container max-w-7xl mx-auto px-6 py-6 z-50">
-			<nav class="flex items-center justify-between font-bold text-white z-50">
+		<div class="tw-container tw-max-w-7xl tw-mx-auto tw-px-6 tw-py-6 tw-z-50">
+			<nav class="tw-flex tw-items-center tw-justify-between tw-font-bold tw-text-white tw-z-50">
 				<NuxtImg
 					src="./logo-white-cropped.png"
-					class="z-10 h-20 w-44 ml-2 -mt-4"
+					class="tw-z-10 tw-h-20 tw-w-44 tw-ml-2 -tw-mt-4"
 				></NuxtImg>
-				<div class="hidden h-10 font-alata mt-2 md:flex md:space-x-6 z-10">
+				<div class="tw-hidden tw-h-10 tw-font-alata tw-mt-2 md:tw-flex md:tw-space-x-6 tw-z-10">
 					<div class="group">
 						<NuxtLink
-							class="text-white no-underline text-2xl font-semibold z-10"
+							class="tw-text-white tw-no-underline tw-text-2xl tw-font-semibold tw-z-10"
 							to="/"
 							>Home</NuxtLink
 						>
 						<div
-							class="opacity-0 group-hover:opacity-100 mt-1 duration-200 group-hover:border-b group-hover:border-blue-50"
+							class="tw-opacity-0 group-hover:tw-opacity-100 tw-mt-1 tw-duration-200 group-hover:tw-border-b group-hover:tw-border-blue-50"
 						></div>
 					</div>
 					<div class="group">
 						<NuxtLink
-							class="text-white no-underline text-2xl font-semibold z-10"
+							class="tw-text-white tw-no-underline tw-text-2xl tw-font-semibold tw-z-10"
 							to="/about"
 							>About</NuxtLink
 						>
 						<div
-							class="opacity-0 group-hover:opacity-100 mt-1 duration-200 group-hover:border-b group-hover:border-blue-50"
+							class="tw-opacity-0 group-hover:tw-opacity-100 tw-mt-1 tw-duration-200 group-hover:tw-border-b group-hover:tw-border-blue-50"
 						></div>
 					</div>
 					<div class="group">
 						<NuxtLink
-							class="text-white no-underline text-2xl font-semibold z-10"
+							class="tw-text-white tw-no-underline tw-text-2xl tw-font-semibold tw-z-10"
 							to="/contact"
 							>Contact</NuxtLink
 						>
 						<div
-							class="opacity-0 group-hover:opacity-100 mt-1 duration-200 group-hover:border-b group-hover:border-blue-50"
+							class="tw-opacity-0 group-hover:tw-opacity-100 tw-mt-1 tw-duration-200 group-hover:tw-border-b group-hover:tw-border-blue-50"
 						></div>
 					</div>
 					<div class="group">
 						<NuxtLink
-							class="text-white pb-2 no-underline text-2xl font-semibold z-10 border-2 rounded-lg px-4 py-1 hover:bg-white hover:text-black duration-200"
+							class="tw-text-white tw-pb-2 tw-no-underline tw-text-2xl tw-font-semibold tw-z-10 tw-border-2 tw-rounded-lg tw-px-4 tw-py-1 hover:tw-bg-white hover:tw-text-black tw-duration-200"
 							to="/login"
 							>Login</NuxtLink
 						>
 					</div>
 					<div class="group">
 						<NuxtLink
-							class="text-white border-2 rounded-lg px-4 py-1 hover:bg-white hover:text-black duration-200 pb-2 no-underline text-2xl font-semibold z-10"
+							class="tw-text-white border-2 tw-rounded-lg tw-px-4 tw-py-1 hover:tw-bg-white hover:tw-text-black tw-duration-200 tw-pb-2 tw-no-underline tw-text-2xl tw-font-semibold tw-z-10"
 							to="/signup"
 							>Sign Up</NuxtLink
 						>
 					</div>
 				</div>
-				<div class="md:hidden z-50 mt-6">
+				<div class="md:tw-hidden tw-z-50 tw-mt-6">
 					<button
 						id="menu-btn"
 						type="button"
-						class="z-50 hamburger md:hidden focus:outline-none"
+						class="tw-z-50 tw-hamburger md:tw-hidden focus:tw-outline-none"
 					>
-						<span class="hamburger-top z-40"></span>
-						<span class="hamburger-middle z-40"></span>
-						<span class="hamburger-bottom z-40"></span>
+						<span class="hamburger-top tw-z-40"></span>
+						<span class="hamburger-middle tw-z-40"></span>
+						<span class="hamburger-bottom tw-z-40"></span>
 					</button>
 				</div>
 			</nav>
-			<div class="text-white text-center my-10 z-40 ">
-				<div class="text-4xl font-bold tracking-wide leading-tight font-sans z-10 columns-2">
-                    <div class="rounded-lg ml-10">
+			<div class="tw-text-white tw-text-center tw-my-10 tw-z-40 ">
+				<div class="tw-text-4xl tw-font-bold tw-tracking-wide tw-leading-tight tw-font-sans tw-z-10 tw-columns-2">
+                    <div class="tw-rounded-lg tw-ml-10">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.465309475045!2d4.352545375594045!3d50.82254456050399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c4f5f0c498b9%3A0x752576890080cd48!2sElectric%20City!5e0!3m2!1sfr!2sbe!4v1682519969362!5m2!1sfr!2sbe" 
                             width="550" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                         </iframe>
                     </div>
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 float-right mr-10">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div class="tw-w-full tw-bg-white tw-rounded-lg tw-shadow dark:tw-border md:tw-mt-0 sm:tw-max-w-md xl:tw-p-0 dark:tw-bg-gray-800 dark:tw-border-gray-700 tw-float-right tw-mr-10">
+                        <div class="tw-p-6 tw-space-y-4 md:tw-space-y-6 sm:tw-p-8">
                             <h1
-                                class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                                class="tw-text-xl tw-font-bold tw-leading-tight tw-tracking-tight tw-text-gray-900 md:tw-text-2xl dark:tw-text-white"
                             >
                                 Contact us
                             </h1>
-                            <form class="space-y-4 md:space-y-6" action="https://formsubmit.co/ba.huwart@gmail.com" method="POST">
+                            <form class="tw-space-y-4 md:tw-space-y-6" action="https://formsubmit.co/ba.huwart@gmail.com" method="POST">
                                 <div>
                                     <label
                                         for="message"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        class="tw-block tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 dark:tw-text-white"
                                         >Your request</label
                                     >
                                     <input
                                         type="text"
                                         name="message"
                                         id="message"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-20 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="tw-bg-gray-50 tw-border tw-border-gray-300 tw-text-gray-900 sm:tw-text-sm tw-rounded-lg focus:tw-ring-primary-600 focus:tw-border-primary-600 tw-block tw-w-full tw-p-20 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-blue-500 dark:focus:tw-border-blue-500"
                                         required="true"
                                     />
                                     <input type="hidden" name="_captcha" value="false">
                                 </div>
-                                <div class="flex items-center justify-between">
+                                <div class="tw-flex tw-items-center tw-justify-between">
                                 </div>
                                 <button
                                     type="submit"
-                                    class="w-full text-white bg-blue-600 hover:bg-slate-900 duration-200 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                    class="tw-w-full tw-text-white tw-bg-blue-600 hover:tw-bg-slate-900 tw-duration-200 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-slate-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-5 tw-py-2.5 tw-text-center"
                                     onclick="alert('the message was sent')"
                                 >
                                     Send
