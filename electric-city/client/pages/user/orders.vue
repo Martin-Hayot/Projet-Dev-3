ef99ed2f6f1a6af1a4005f1ab12a9d3dc50d5fa0
antoinegeonet <he202020@students.ephec.be>
2023-05-11 10:58:37 +0200

    Mail de confirmation apres l enregistrement d une commande + popup de confirmation en front

--- electric-city/client/pages/user/orders.vue
+++ electric-city/client/pages/user/orders.vue
@@ -2,7 +2,6 @@
 	<NuxtLayout>
 		<div class="box">
 			<h1>Order history</h1>
-			<button class="bouton">Order now</button>
 			<table>
 				<thead>
 					<tr>
@@ -19,14 +18,43 @@
 					</tr>
 				</tbody>
 			</table>
+			<div class="text-center">
+				<v-btn
+						color="blue"
+						@click="mailOrderConfirmation"
+				>
+					Order Now
+				</v-btn>
+			</div>
+		</div>
+		<div class="text-center">
+
+			<v-dialog
+					v-model="dialog"
+					width="auto"
+			>
+				<v-card>
+					<v-card-text>
+						Un mail de confirmation avec les détails de votre commande vous a été envoyé par email.
+					</v-card-text>
+					<v-card-text>
+						Si le mail n'est pas arrivé dans les 10 minutes suivant la commande, prenez contact par téléphone avec nous.
+					</v-card-text>
+					<v-card-actions>
+						<v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
 		</div>
 	</NuxtLayout>
 </template>
 
 <script>
+	import {tr} from "vuetify/locale";
 	export default {
 		data() {
 			return {
+				dialog: false,
 				commandes: [
 					{
 						id: 1,
@@ -65,6 +93,20 @@
 			formatDate(date) {
 				const options = { year: "numeric", month: "long", day: "numeric" };
 				return new Date(date).toLocaleDateString("fr-FR", options);
+			},
+			async mailOrderConfirmation() {
+				// POST request using fetch with async/await
+				const requestOptions = {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						userId: "Antoine",
+						orderId: 1423
+					})
+				};
+				const response = await fetch("http://localhost:3001/api/order/mail", requestOptions);
+				//const data = response.json();
+				this.dialog = true;
 			},
 		},
 	};
--- electric-city/server/server.js
+++ electric-city/server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const auth = require("./routes/auth");
 const command = require("./routes/user");
+const mail = require("./routes/mail");
 const [authenticateToken, generateAccessToken] = require("./middleware/auth");
 const multer = require("multer");
 const upload = multer({ dest: "storage/" });
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/auth", auth);
 app.use("/api/user", command);
+app.use("/api/order", mail);
 
 app.post(
 	"/api/upload",
