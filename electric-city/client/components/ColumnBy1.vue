e89a95b6f6c2036329119200b9e4a27b030c3284
antoinegeonet <he202020@students.ephec.be>
2023-05-25 11:53:12 +0200

    ajout de la page about

---
+++ electric-city/client/components/ColumnBy1.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="tw-bg-black tw-text-white">
+        <div class="tw-p-10 tw-grid tw-grid-cols-1 tw-gap-8">
+            <div class="tw-m-auto">
+                <slot />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "ColumnsBy2"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ electric-city/client/components/ColumnsBy2.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="tw-bg-black tw-text-white">
+        <div class="tw-p-10 tw-grid md:tw-grid-cols-2 tw-gap-8">
+            <div class="tw-m-auto">
+                <slot name="column1" />
+            </div>
+            <div class="tw-m-auto">
+                <slot name="column2" />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ColumnsBy2"
+}
+</script>
+
+<style scoped>
+
+</style>
