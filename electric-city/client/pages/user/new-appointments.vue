21c88b57728411272d26f6d9cf074577507139e7
Martin-Hayot <m.hayot@students.ephec.be>
2023-05-28 16:43:17 +0200

    bug fix

--- electric-city/client/pages/user/new-appointments.vue
+++ electric-city/client/pages/user/new-appointments.vue
@@ -1,7 +1,9 @@
 <template>
-	<NuxtLayout>
-		<Calendar></Calendar>
-	</NuxtLayout>
+	<ClientOnly>
+		<NuxtLayout>
+			<Calendar></Calendar>
+		</NuxtLayout>
+	</ClientOnly>
 </template>
 
 <script setup>
