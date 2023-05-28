57b8d8f9c1aeb9548b77016fd92e31d2efbe4732
antoinegeonet <he202020@students.ephec.be>
2023-05-28 09:32:54 +0200

    ajout de la page about

--- electric-city/client/components/ColumnBy1.vue
+++ electric-city/client/components/ColumnBy1.vue
@@ -1,10 +1,16 @@
 <template>
-$END$
+    <section class="tw-bg-black tw-text-white">
+        <div class="tw-p-10 tw-grid tw-grid-cols-1 tw-gap-8">
+            <div class="tw-m-auto">
+                <slot />
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-name: "ColumnBy1"
+    name: "ColumnsBy2"
 }
 </script>
 
--- electric-city/client/components/ColumnsBy2.vue
+++ electric-city/client/components/ColumnsBy2.vue
@@ -1,10 +1,19 @@
 <template>
-$END$
+    <section class="tw-bg-black tw-text-white">
+        <div class="tw-p-10 tw-grid md:tw-grid-cols-2 tw-gap-8">
+            <div class="tw-m-auto">
+                <slot name="column1" />
+            </div>
+            <div class="tw-m-auto">
+                <slot name="column2" />
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-name: "ColumnsBy2"
+    name: "ColumnsBy2"
 }
 </script>
 
