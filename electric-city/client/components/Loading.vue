8fc95b6bf7de8032a40c525ece9777899f1f46f0
Martin-Hayot <m.hayot@students.ephec.be>
2023-06-05 15:15:35 +0200

    not important

---
+++ electric-city/client/components/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+	<div
+		class="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-screen"
+	>
+		<NuxtImg src="/logo.png" class="tw-h-44 tw-w-44"> </NuxtImg>
+		<v-progress-circular indeterminate></v-progress-circular>
+	</div>
+</template>
