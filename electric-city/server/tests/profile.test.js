ffbd67e0725f59f61d43293892617dc374f53a79
Gaetan2002 <gaetan.leplae44@gmail.com>
2023-06-06 13:20:17 +0200

    rajout de deux testing pour la route get / de profile un test avant et apres modification du profile

--- electric-city/server/tests/profile.test.js
+++ electric-city/server/tests/profile.test.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 const db = require("../utils/db.server.ts");
 let oldAccessToken = "";
+let newAccessToken = "";
 
 beforeAll(async () => {
+	const deleteUser = await db.User.deleteMany({});
 	const responseUser = await axios.post(
 		"http://localhost:3001/api/auth/signup",
 		{
@@ -15,18 +17,29 @@ beforeAll(async () => {
 	const responseLoginOld = await axios.post(
 		"http://localhost:3001/api/auth/login",
 		{
-			email: "test@example.com",
+			email: responseUser.data.email,
 			password: "password123",
 		}
 	);
 	oldAccessToken = responseLoginOld.data.accessToken;
 });
 
-afterAll(async () => {
-	const deleteUser = await db.User.deleteMany({});
+afterAll(async () => {});
+
+describe("road to profile", () => {
+	test("GET /", async () => {
+		const response = await axios.get("http://localhost:3001/api/profile/", {
+			headers: { Authorization: oldAccessToken },
+		});
+		expect(response.data).toEqual({
+			email: "test@example.com",
+			firstname: "jhon",
+			lastname: "Doe",
+		});
+	});
 });
 
-describe("road to orders", () => {
+describe("road to profile", () => {
 	test("PUT /status/edit", async () => {
 		const response = await axios.put("http://localhost:3001/api/profile/edit", {
 			email: "test@example.be",
@@ -35,10 +48,24 @@ describe("road to orders", () => {
 			password: "password321",
 			accessToken: oldAccessToken,
 		});
+		newAccessToken = response.data.accessToken;
 		expect(response.data).toEqual({
 			accessToken: expect.any(String),
 			email: "test@example.be",
 			user_type: "USER",
+			firstname: "quentin",
+			lastname: "dupont",
+		});
+	});
+});
+
+describe("road to profile", () => {
+	test("GET /", async () => {
+		const response = await axios.get("http://localhost:3001/api/profile/", {
+			headers: { Authorization: newAccessToken },
+		});
+		expect(response.data).toEqual({
+			email: "test@example.be",
 			firstname: "quentin",
 			lastname: "dupont",
 		});
